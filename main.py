d2cab0b8a0de71ba9e74a92ae56412a6a02649df
Iwo Dudkiewicz <iwo.dudkiewicz@gmail.com>
2025-06-11 23:20:14 +0200

    add basic discord intents

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import os
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='urf-8', mode='w') #  możliwe, że mode='a'
+intenst = discord.Intents.default()
+intenst.message_content = True
+intenst.members = True
