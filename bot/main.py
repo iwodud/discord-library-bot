95c5452983a4d956f996248025aad29ce662a706
Iwo Dudkiewicz <iwo.dudkiewicz@gmail.com>
2025-06-14 02:16:59 +0200

    copy main.py to bot/main.py and delete main.py

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,26 @@ import os
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-handler = logging.FileHandler(filename='discord.log', encoding='urf-8', mode='w') #  możliwe, że mode='a'
-intenst = discord.Intents.default()
-intenst.message_content = True
-intenst.members = True
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w') #  możliwe, że mode='a'
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"The {bot.user.name} is ready")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    await bot.process_commands(message)
+
+
+
+bot.run(token)
