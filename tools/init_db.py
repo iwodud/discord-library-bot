86507774232f7034ad0987bc3aedec33ca100ecf
Iwo Dudkiewicz <iwo.dudkiewicz@gmail.com>
2025-06-17 01:22:04 +0200

    Move schema.sql, example_data.py and init_db.py to tools folder

--- tools/init_db.py
+++ tools/init_db.py
@@ -4,8 +4,8 @@
 import sqlite3
 from pathlib import Path
 
-SCHEMA_PATH = Path("database/schema.sql")
-DB_PATH = Path("library.db")
+SCHEMA_PATH = Path("tools/schema.sql")
+DB_PATH = Path("../library.db") if __name__ == "__main__" else Path("library.db")
 
 def init_db():
     conn = sqlite3.connect(DB_PATH)
