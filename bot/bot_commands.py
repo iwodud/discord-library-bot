4e60ba25e9b9c0764567a30e950f71e4f0802a59
Iwo Dudkiewicz <iwo.dudkiewicz@gmail.com>
2025-06-26 00:23:16 +0200

    change prefix-commands for slash-commands

--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -1,16 +1,22 @@
+from discord import Interaction
 from discord.ext import commands
-
 from database.db import get_all_books, format_books
 
 
-def register_commands(bot):
-    '''After using a command on discord, the bot says "hello [users name]!"'''
-    @bot.command()
-    async def hello(ctx):
-        await ctx.send(f'Hello {ctx.author.mention}!')
+def register_slash_commands(bot: commands.Bot):
+    @bot.tree.command(name="hello", description="Przywitaj się z botem")
+    async def hello(interaction: Interaction):
+        '''After using a command on discord, the bot says "hello [users name]!"'''
+        await interaction.response.send_message(f"Hello {interaction.user.mention}!")
     
 
-    @bot.command()
-    async def show_books(ctx):
+    @bot.tree.command(name="show_books", description="Pokaż wszystkie książki w bibliotece")
+    async def show_books(interaction: Interaction):
         books = get_all_books()
-        await ctx.send(format_books(books))
+        message = format_books(books)
+        await interaction.response.send_message(message)
+    
+
+    @bot.tree.command(name="ping")
+    async def ping(interaction: Interaction):
+        await interaction.response.send_message("pong!")
--- bot/main.py
+++ bot/main.py
@@ -17,20 +17,31 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-bot_commands.register_commands(bot)
+bot_commands.register_slash_commands(bot)
 
 
 @bot.event
 async def on_ready():
+    GUILD_ID = 1380933352131002408
+    guild = discord.Object(id=GUILD_ID)
+    await bot.tree.sync(guild=guild)
+    await bot.tree.sync()
     print(f"The {bot.user.name} is ready")
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
     
-    await bot.process_commands(message)
+#     await bot.process_commands(message)
 
 
 bot.run(token)
+
+    # try:
+    #     guild = discord.Object(id=1380933352131002408)
+    # except Exception as e:
+    #     print(f'Error syncing cimmandx: {e}')
+
+    # GUILD_ID = discord.Object(id=1380933352131002408)
